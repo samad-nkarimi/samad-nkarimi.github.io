f2e063189e2660f7c2b69cbd70ecd36f3fbb18ea
samad karimi <samad.nkarimi@gmail.com>
2022-07-23 20:04:36 +0430

    part1 ex1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,31 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <ol>
+      <li>
+        {props.parts[0]} {props.exercises[0]}
+      </li>
+      <li>
+        {props.parts[1]} {props.exercises[1]}
+      </li>
+      <li>
+        {props.parts[2]} {props.exercises[2]}
+      </li>
+    </ol>
+  );
+};
+
+const Footer = (props) => {
+  return (
+    <p>
+      {props.text} {props.exercises}
+    </p>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +37,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Footer
+        text="Number of exercises"
+        exercises={exercises1 + exercises2 + exercises3}
+      />
     </div>
   );
 };
