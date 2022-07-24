019f8c68fcdd6c98f26430d0c47f2595f0da6af6
samad karimi <samad.nkarimi@gmail.com>
2022-07-24 23:08:50 +0430

    part2-2 add note

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,71 +1,39 @@
 import { useState } from "react";
+import Note from "./components/Note";
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
-const Content = (props) => {
-  return (
-    <ol>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </ol>
-  );
-};
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
 
-const Part = (props) => {
-  return (
-    <li>
-      {props.part.name} {props.part.exercises}
-    </li>
-  );
-};
-
-const Total = (props) => {
-  const text = "Number of exercises";
-  const exercises =
-    props.parts[0].exercises +
-    props.parts[1].exercises +
-    props.parts[2].exercises;
-  return (
-    <p>
-      {text} {exercises}
-    </p>
-  );
-};
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  // setTimeout(() => setCounter(counter + 1), 1000);
-
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-  const parts = [part1, part2, part3];
-  const course = {
-    name: "Half Stack application development",
-    parts: parts,
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
   };
 
   return (
     <div>
-      <h2>{counter}</h2>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
