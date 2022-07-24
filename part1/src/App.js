a5a7b8099c1b43e255a435a38b6eeb427da35997
samad karimi <samad.nkarimi@gmail.com>
2022-07-25 00:57:55 +0430

    part2.2 ex 2.6-2.10

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,48 +1,101 @@
 import { useState } from "react";
-import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
+const Person = ({ person }) => {
+  return (
+    <li>
+      {person.name}
+      {person.number}
+    </li>
+  );
+};
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    };
+const Filter = ({ value, handler }) => {
+  return (
+    <div>
+      search: <input value={value} onChange={handler} />
+    </div>
+  );
+};
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  };
-  const handleNoteChange = (event) => {
+const Form = ({
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+  saveNewName,
+}) => {
+  return (
+    <form onSubmit={(event) => event.preventDefault()}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit" onClick={saveNewName}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
+  var personsToShow = persons.filter((p) =>
+    p.name.toLowerCase().match(searchValue.toLowerCase())
+  );
+
+  const handleNameChange = (event) => {
     console.log(event.target.value);
-    setNewNote(event.target.value);
+    setNewName(event.target.value);
   };
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  const saveNewName = () => {
+    if (persons.find((p) => p.name === newName)) {
+      alert("name already exists");
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </div>
+      <h2>Phonebook</h2>
+      <Filter value={searchValue} handler={handleSearchChange} />
+      <h2>add a new</h2>
+      <Form
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+        saveNewName={saveNewName}
+      />
+      <h2>Persons</h2>
       <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+        {personsToShow.map((person) => (
+          <Person key={person.name} person={person} />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
     </div>
   );
 };
