1dad06c681c2d2e17f4c4dc02854055f5fbaaba9
samad karimi <samad.nkarimi@gmail.com>
2022-07-25 16:36:18 +0430

    part2-3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,103 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
-const Person = ({ person }) => {
-  return (
-    <li>
-      {person.name}
-      {person.number}
-    </li>
-  );
-};
-
-const Filter = ({ value, handler }) => {
-  return (
-    <div>
-      search: <input value={value} onChange={handler} />
-    </div>
-  );
-};
-
-const Form = ({
-  newName,
-  handleNameChange,
-  newNumber,
-  handleNumberChange,
-  saveNewName,
-}) => {
-  return (
-    <form onSubmit={(event) => event.preventDefault()}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit" onClick={saveNewName}>
-          add
-        </button>
-      </div>
-    </form>
-  );
-};
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [searchValue, setSearchValue] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
 
-  var personsToShow = persons.filter((p) =>
-    p.name.toLowerCase().match(searchValue.toLowerCase())
-  );
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  };
-  const handleNumberChange = (event) => {
-    console.log(event.target.value);
-    setNewNumber(event.target.value);
-  };
-  const handleSearchChange = (event) => {
-    console.log(event.target.value);
-    setSearchValue(event.target.value);
-  };
-
-  const saveNewName = () => {
-    if (persons.find((p) => p.name === newName)) {
-      alert("name already exists");
-    } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName("");
-      setNewNumber("");
-    }
-  };
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter value={searchValue} handler={handleSearchChange} />
-      <h2>add a new</h2>
-      <Form
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-        saveNewName={saveNewName}
-      />
-      <h2>Persons</h2>
-      <ul>
-        {personsToShow.map((person) => (
-          <Person key={person.name} person={person} />
-        ))}
-      </ul>
-    </div>
-  );
+  // ...
 };
-
-export default App;
