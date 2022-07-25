6d644873dd441c760141a02443e7b0facbc6c620
samad karimi <samad.nkarimi@gmail.com>
2022-07-25 16:49:32 +0430

    part2-3-ex

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Person = ({ person }) => {
   return (
@@ -41,15 +42,20 @@ const Form = ({
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchValue, setSearchValue] = useState("");
+
+  const getData = () => {
+    console.log("effect");
+    axios.get("http://localhost:3002/persons").then((response) => {
+      console.log("response");
+      setPersons(response.data);
+    });
+  };
+  useEffect(getData, []);
+  console.log("render", persons.length, "persons");
 
   var personsToShow = persons.filter((p) =>
     p.name.toLowerCase().match(searchValue.toLowerCase())
